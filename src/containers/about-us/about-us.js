9dc3ef09890918e931676fc14586db39e960c4d7
chetankb <chetankb619@gmail.com>
2024-06-19 23:57:13 +0800

    Updated Map location

--- src/containers/about-us/about-us.js
+++ src/containers/about-us/about-us.js
@@ -21,7 +21,7 @@ const AboutUs = () => {
 			<div className='about-us-content-map'>
 				<div className='map'>
 					<iframe
-						src='https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3887.37697833258!2d77.67902699999999!3d13.011649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTPCsDAwJzQxLjkiTiA3N8KwNDAnNDQuNSJF!5e0!3m2!1sen!2smy!4v1712975531570!5m2!1sen!2smy'
+						src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.377989584736!2d77.6790041!3d13.0115845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae110034629701%3A0xe694ad774e05f3a7!2sUrbanMES!5e0!3m2!1sen!2smy!4v1718812424836!5m2!1sen!2smy'
 						width='100%'
 						height='450'
 						style={{ border: 0, outline: 'none' }}
